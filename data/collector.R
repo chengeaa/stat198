ca8eb4c0ad1a85a5b6d4daf9ca3d3e2bc9481677
Erik Cheng <ckaiwen2@hotmail.com>
2017-04-25 14:29:07 -0700

    updated

--- data/collector.R
+++ data/collector.R
@@ -5,7 +5,7 @@ library('twitteR')
 require(readr) 
 require(RJSONIO)
 
-data = read.delim(file = "all_streaming_api.txt", sep = "\n", header = FALSE, colClasses = c("character"))
+data = read.delim(file = "all_firehose.txt", sep = "\n", header = FALSE, colClasses = c("character"))
 #A function for getting a tweet object given a TweetID (as string)
 getTweet = function(k){
   out = (tryCatch(
@@ -33,17 +33,17 @@ getTime = function(tweet) { #takes in tweet object and returns creation time
 ###LETS GET CRANKIN###
 ######################
 
-low = 51001
-high = 52000
+low = 1
+high = 301
 lasttime  = Sys.time()
 
 while(low < length(data[,1])) {
   currtime = Sys.time()
-  if((round(difftime(currtime, lasttime, units = "mins")) >= 15) || low == 51001){
+  if((round(difftime(currtime, lasttime, units = "mins")) >= 15) || low == 1){
     if(high > length(data[,1])) {
       high = length(data[,1])
     }
-    setup_twitter_oauth("vRb1BPv0mp85t5uk99MD5GdvY","vbEKOCu58TnGx3EKTmGyAV9zZ08nRYUCDvTrDfcw7dTR7Snf8k","2934227960-io1QWn50I2n5D9tpwp8ZQittP9cVsBsGdEBCPRp", "tlaDmitkS228IOPy25VLlnZWP4PFnpIiVuaZgeeoLKkL9")  
+    setup_twitter_oauth("KEYS REDACTED")  
     fullTweets = sapply(c(low:high), getTweet)
     nulls = which(unlist(lapply(fullTweets,is.null)))
     fullTweetsdf = twListToDF(fullTweets[-nulls])
@@ -51,7 +51,7 @@ while(low < length(data[,1])) {
     write.table(as.data.frame(absnulls), file = paste("errorsfrom",as.character(low),".csv"), sep = ",")
     write.table(fullTweetsdf, file = paste("tweetsfrom", as.character(low),".csv"), sep = ",")
     low = high + 1
-    high = high + 1000
+    high = high + 300
   }
   
 }
